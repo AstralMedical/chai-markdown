6fea8f57ad6177516cf0c44c9cd1f3b64352a0e5
danprince <danielprince@hotmail.co.uk>
2015-07-09 11:40:27 +0100

    adding tests and fixing bugs

--- markdown.js
+++ markdown.js
@@ -2,14 +2,14 @@ angular.module('chai.markdown', ['ngSanitize'])
 
 .service('MarkdownTransform', function($sanitize) {
   this.linkTarget = function(destination, html) {
-    return html.replace(/<a /, '<a target="' + destination + '"');
+    return html.replace(/<a /, '<a target="' + destination + '" ');
   };
 
   this.sanitize = $sanitize;
 })
 
 .directive('chaiMarkdown', function(MarkdownTransform) {
-  var converter = new Showdown.converter();
+  var converter = new showdown.Converter();
 
   return {
     restrict: 'A',
@@ -26,7 +26,7 @@ angular.module('chai.markdown', ['ngSanitize'])
           .map(converter.makeHtml)
           .map(MarkdownTransform.linkTarget.bind(null, '_blank'))
           .map(MarkdownTransform.sanitize)
-          .map(element.html);
+          .map(element.html.bind(element));
       });
     }
   };
