e4ea5778bbd41e158574cceb39cba8cc06f661fa
danprince <danielprince@hotmail.co.uk>
2015-07-09 11:30:18 +0100

    simple wrapper for component

---
+++ markdown.js
@@ -0,0 +1,34 @@
+angular.module('chai.markdown', ['ngSanitize'])
+
+.service('MarkdownTransform', function($sanitize) {
+  this.linkTarget = function(destination, html) {
+    return html.replace(/<a /, '<a target="' + destination + '"');
+  };
+
+  this.sanitize = $sanitize;
+})
+
+.directive('chaiMarkdown', function(MarkdownTransform) {
+  var converter = new Showdown.converter();
+
+  return {
+    restrict: 'A',
+    scope: {
+      chaiMarkdown: '='
+    },
+    link: function(scope, element) {
+      scope.$watch('chaiMarkdown', function(markdown) {
+        if(!markdown) return;
+
+        // this is what happens when js provides no composition operators.
+        // :(
+        [markdown]
+          .map(converter.makeHtml)
+          .map(MarkdownTransform.linkTarget.bind(null, '_blank'))
+          .map(MarkdownTransform.sanitize)
+          .map(element.html);
+      });
+    }
+  };
+});
+
